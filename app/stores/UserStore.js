45a0ea8050459ac71f0627cc5e13bbafd160d8c7
xiaoyaomaopp <xiaoyaomaopp@sohuc.om>
2017-01-17 18:54:01 +0800

    增加微信登入

---
+++ app/stores/UserStore.js
@@ -0,0 +1,33 @@
+import alt from '../alt';
+import UserStoreActions from '../actions/UserStoreActions';
+
+class UserStore {
+    constructor() {
+        this.bindActions(UserStoreActions);
+        this.data = {
+            openid: "",
+            sex: 1,
+            languag: 'zh_CN',
+            city: '苏州',
+            province: '江苏',
+            country: '中国',
+            headimgurl: "",
+            nickname: "",
+            collect: [],
+            marking: {},
+            privilege: []
+        };
+    }
+
+    onSuccess(data) {
+        console.log("UserStore", "onSuccess");
+        this.data = data;
+        //console.log(data);
+    }
+
+    onFailure(error) {
+        toastr.error(error);
+    }
+}
+
+export default alt.createStore(UserStore);
